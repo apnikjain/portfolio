1526f171e80fbc78cfd53614b70401aefea1c463
MY HP <apnikmkjain@gmail.com>
2021-05-24 02:33:09 +0530

    Updates

---
+++ precache-manifest.e2591406e8ea43ed5cce854a0ac77159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa0ac5fd1e5db52732d8fa4c9f638cc",
+    "url": "/apnikjain-portfolio/index.html"
+  },
+  {
+    "revision": "ca94681ffd4139b7d725",
+    "url": "/apnikjain-portfolio/static/css/2.e7de8650.chunk.css"
+  },
+  {
+    "revision": "3fff7686e37c9482e98a",
+    "url": "/apnikjain-portfolio/static/css/main.29a8aa67.chunk.css"
+  },
+  {
+    "revision": "ca94681ffd4139b7d725",
+    "url": "/apnikjain-portfolio/static/js/2.22579718.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/apnikjain-portfolio/static/js/2.22579718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fff7686e37c9482e98a",
+    "url": "/apnikjain-portfolio/static/js/main.cccec54a.chunk.js"
+  },
+  {
+    "revision": "85cf9a3a82575fb4b5ed",
+    "url": "/apnikjain-portfolio/static/js/runtime-main.7c2c1447.js"
+  }
+]);
